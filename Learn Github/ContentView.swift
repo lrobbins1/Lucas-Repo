6bf43dbd859c0c8552b61afc916c5331ea1526aa
Lucas Robbins <altairboard@gmail.com>
2022-08-19 16:58:13 -0600

    Initial Commit

---
+++ Learn Github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn Github
+//
+//  Created by Lucas Robbins on 8/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn Github/Learn_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_GithubApp.swift
+//  Learn Github
+//
+//  Created by Lucas Robbins on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Learn_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
